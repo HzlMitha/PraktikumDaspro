2a3fdb44b4c0bcf8815c5e0ac0a3ecbce795bf4c
Dian Paramitha <dianmitha89031@gmail.com>
2024-09-06 09:42:54 +0700

    Praktikum01

---
+++ Praktikum01/Latihan05.java
@@ -0,0 +1,10 @@
+/**
+ * Latihan05
+ */
+public class Latihan05 {
+
+    public static void main(String[] args) {
+        System.out.print("Nama saya Mitha");
+        System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+    }
+}
---
+++ Praktikum01/Tugas05.java
@@ -0,0 +1,19 @@
+/**
+ * Tugas05
+ */
+public class Tugas05 {
+
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        String nama = "Dian Paramitha";
+        String absen = "05";
+        String kelas = "1D";
+        String prodi = "Sistem Informasi Bisnis";
+        String jurusan = "Teknologi Informasi";
+        System.out.println("Nama\t: " + nama);
+        System.out.println("Absen\t: " + absen);
+        System.out.println("Kelas\t: " + kelas);
+        System.out.println("Prodi\t: " + prodi);
+        System.out.println("Jurusan\t: " + jurusan);
+    }
+}
