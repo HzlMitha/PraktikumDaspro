111b0e9cbe1e34f3bfc45273ed488fd5af0d6df3
Dian Paramitha <dianmitha89031@gmail.com>
2024-11-10 19:24:58 +0700

    Tugas01

--- Praktikum09/SearchNilai05.java
+++ Praktikum09/SearchNilai05.java
@@ -1,21 +1,67 @@
 package Praktikum09;
+import java.util.Scanner;
 
 public class SearchNilai05 {
     public static void main(String[] args) {
-        
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc05 = new Scanner(System.in);
 
+        System.out.print("Masukkan jumlah elemen array: ");
+        int i = sc05.nextInt();
+    
+        int[] arrNilai = new int[i];
+
+        for (int b = 0; b < i; b++) {
+            System.out.print("Masukkan nilai mahasiswa ke- " + (b+1) + ": ");
+            arrNilai[b] = sc05.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc05.nextInt();
+
+        int index = linearSearch(arrNilai, key);
+        
+        if (index != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (index+1));
+        } else {
+            System.out.println("Nilai " + key + "tidak ditemukan");
+        }
+
+        //int hasil = 0;
+
+        // for (int a = 0; i < arrNilai.length; a++) {
+        //     if (key == arrNilai[i]) {
+        //         hasil = i;
+        //         break;
+        //     } else {
+        //         System.out.println("Nilai yang dicari tidak ditemukan");
+        //         return;
+        //     }
+        // }
+
+        
+        // int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
+        // int key = 90;
+        // int hasil = 0;
+
+        // for (int i = 0; i < arrNilai.length; i++) {
+        //     if (key == arrNilai[i]) {
+        //         hasil = i;
+        //         break;
+        //     }
+        // }
+        
+        // System.out.println();
+        // System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
+        // System.out.println();
+        sc05.close();
+    }
+
+    public static int linearSearch(int[] arrNilai, int key) {
         for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
-                break;
+            if (arrNilai[i] == key) {
+                return i;
             }
         }
-        
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        return -1;
     }
 }
---
+++ Praktikum09/Tugas01.java
@@ -0,0 +1,75 @@
+package Praktikum09;
+import java.util.Scanner;
+
+public class Tugas01 {
+    public static void main(String[] args) {
+        Scanner sc05 = new Scanner(System.in);
+
+        String[] daftarMenu = {"Nasi goreng", "Mie goreng", "Roti bakar", "Kentang goreng", "Teh tarik", "Cappucino", "Chocolate ice"};
+        double[] daftarHarga = {20000, 22000, 15000, 17000, 13000, 18000, 20000};
+        
+        System.out.println("--------------------------");
+        System.out.println("====== DAFTAR MENU =======");
+        System.out.println("--------------------------");
+        System.out.println("1.Nasi goreng     Rp 20000");
+        System.out.println("2.Mie goreng      Rp 22000");
+        System.out.println("3.Roti bakar      Rp 15000");
+        System.out.println("4.Kentang goreng  Rp 17000");
+        System.out.println("5.Teh tarik       Rp 13000");
+        System.out.println("6.Cappucino       Rp 18000");
+        System.out.println("7.Chocolate ice   Rp 20000");
+        
+        System.out.print("Masukkan jumlah menu yang akan dipesan: ");
+        int jumlahMenu = sc05.nextInt();
+        sc05.nextLine();
+
+        String[] namaPesanan = new String[jumlahMenu];
+        double[] hargaPesanan = new double[jumlahMenu];
+        int[] jumlahItemPesanan = new int[jumlahMenu];
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            System.out.print("Masukkan menu" + (i+1) + " : ");
+            String pilihanMenu = sc05.nextLine();
+
+            int indexMenu = -1;
+            for (int j = 0; j < daftarMenu.length; j++) {
+                if (daftarMenu[j].equalsIgnoreCase(pilihanMenu)) {
+                    indexMenu = j;
+                    break;
+                }
+            } if (indexMenu != -1) {
+                System.out.print("Masukkan jumlah pesanan untuk " + pilihanMenu + " : ");
+                int jumlah = sc05.nextInt();
+                sc05.nextLine();
+
+                namaPesanan[i] = daftarMenu[indexMenu];
+                hargaPesanan[i] = daftarHarga[indexMenu];
+                jumlahItemPesanan[i] = jumlah;
+
+                System.out.println("Pesanan ditambahkan: " + pilihanMenu + " x" + jumlah);
+            } else {
+                System.out.println("Menu tidak tersedia, silakan pilih menu lain.");
+                i--;
+            }
+
+        }
+
+        double totalHarga = 0;
+        System.out.println();
+        System.out.println("=======Daftar Pesanan Anda=======");
+
+        for (int i = 0; i < jumlahMenu; i++) {
+            if (namaPesanan[i] != null) {
+                double subtotal = hargaPesanan[i] * jumlahItemPesanan[i];
+                totalHarga += subtotal;
+                System.out.println(namaPesanan[i] + " x" + jumlahItemPesanan[i] + " = Rp " + subtotal);
+            }
+        }
+        
+        System.out.println("Total harga yang perlu dibayar: Rp " + totalHarga);
+        
+        sc05.close();
+    }
+
+    
+}
