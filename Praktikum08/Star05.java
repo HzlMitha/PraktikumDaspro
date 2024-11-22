56f6d2e087f91248288fd775f776d039e66c6d5b
Dian Paramitha <dianmitha89031@gmail.com>
2024-11-22 10:11:26 +0700

    Praktikum11

--- Praktikum08/Star05.java
+++ Praktikum08/Star05.java
@@ -8,7 +8,7 @@ public class Star05 {
         System.out.print("Masukkan nilai N = ");
         int N = sc05.nextInt();
 
-        for (int i=1; i> N; i++) {
+        for (int i=1; i > N; i++) {
             System.out.print("*");
         }
 
---
+++ Praktikum11/PengunjungCafe05.java
@@ -0,0 +1,15 @@
+package Praktikum11;
+
+public class PengunjungCafe05 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+
+    }
+
+    public static void daftarPengunjung(String...namaPengunjung) {
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+}
